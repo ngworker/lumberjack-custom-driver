6b48cce1163a79070ad37debed8758d28553ddfc
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2020-12-05 23:52:14 +0100

    chore: update to Angular 11 workspace

--- karma.conf.js
+++ karma.conf.js
@@ -11,13 +11,16 @@ module.exports = () => ({
     require('karma-jasmine'),
     require('karma-chrome-launcher'),
     require('karma-jasmine-html-reporter'),
-    require('karma-coverage-istanbul-reporter'),
+    require('karma-coverage'),
     require('@angular-devkit/build-angular/plugins/karma'),
   ],
   client: {
     clearContext: false, // leave Jasmine Spec Runner output visible in browser
   },
-  coverageIstanbulReporter: {
+  jasmineHtmlReporter: {
+    suppressAll: true, // removes the duplicated traces
+  },
+  coverageReporter: {
     dir: path.join(__dirname, 'coverage'),
     reports: ['html', 'lcovonly', 'text-summary'],
     fixWebpackSourcePaths: true,
